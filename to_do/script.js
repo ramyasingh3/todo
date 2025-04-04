2e4408c3ac8c7da658eafa4f6e4091510974e16d
Ramya Singh <ramya333singh@gmail.com>
2025-04-04 21:41:41 +0530

    Initial commit: Todo list application

---
+++ to_do/script.js
@@ -0,0 +1,21 @@
+document.getElementById('addTaskBtn').addEventListener('click', addTask);
+
+function addTask() {
+    let taskInput = document.getElementById('taskInput');
+    let taskList = document.getElementById('taskList');
+
+    if (taskInput.value.trim() === '') {
+        alert("Please enter a task!");
+        return;
+    }
+
+    let li = document.createElement('li');
+    li.innerHTML = `${taskInput.value} <button onclick="removeTask(this)">Remove</button>`;
+    taskList.appendChild(li);
+
+    taskInput.value = '';
+}
+
+function removeTask(taskElement) {
+    taskElement.parentElement.remove();
+}
