45372c3700f8b7f7cc51216a1e35b1f81121ca8e
Ramya Singh <ramya333singh@gmail.com>
2025-06-07 10:00:00 +0530

    Solved DSA Question 1: Quick Sort

---
+++ DSA/2025_06_07/selection_sort.py
@@ -0,0 +1,14 @@
+# Selection Sort Implementation
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+if __name__ == "__main__":
+    arr = [29, 10, 14, 37, 13]
+    print(selection_sort(arr)) 
