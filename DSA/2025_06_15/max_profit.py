cf725f1c54ad07f334acf1ba41ff96447db2136d
Ramya Singh <ramya333singh@gmail.com>
2025-06-15 10:00:00 +0530

    Solved DSA Question 1: Two Sum

---
+++ DSA/2025_06_15/max_profit.py
@@ -0,0 +1,11 @@
+# Max Profit
+def max_profit(prices):
+    if not prices: return 0
+    min_price = prices[0]
+    max_profit = 0
+    for price in prices:
+        max_profit = max(max_profit, price - min_price)
+        min_price = min(min_price, price)
+    return max_profit
+
+print(max_profit([7,1,5,3,6,4]))
